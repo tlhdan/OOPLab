f9b519615bb54e5e04234a9f2eafd0a898c72cf1
tlhdan <95524572+tlhdan@users.noreply.github.com>
2024-09-27 15:06:25 +0700

    Update ChoosingOption.java

--- Lab01/ChoosingOption.java
+++ Lab01/ChoosingOption.java
@@ -2,7 +2,7 @@ import javax.swing.JOptionPane;
 public class ChoosingOption {
 	public static void main(String[] args){
 		int option = JOptionPane.showConfirmDialog(null,
-				"Do you want to change to the first class ticket?", "Select an Option", JOptionPane.YES_NO_OPTION);
+				"Do you want to change to the first class ticket?");
 		JOptionPane.showMessageDialog(null, "You've chosen: "
 				+ (option==JOptionPane.YES_OPTION?"Yes":"No"));
 		System.exit(0);
